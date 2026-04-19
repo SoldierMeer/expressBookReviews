7c3747b4da8b62404c7976051e3091cdc58bf9ce
SoldierMeer <meeransari24@gmail.com>
2026-04-19 07:23:54 -0400

    add authentication

--- final_project/index.js
+++ final_project/index.js
@@ -10,8 +10,25 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+    if (req.session.authorization) {
+        let token = req.session.authorization['accessToken'];
+
+        if (token) {
+            jwt.verify(token, "fingerprint_customer", (err, user) => {
+                if (!err) {
+                    req.user = user;
+                    next();
+                } else {
+                    return res.status(403).json({ message: "User not authorized" });
+                }
+            });
+        } else {
+            return res.status(403).json({ message: "Token missing" });
+        }
+    } else {
+        return res.status(403).json({ message: "User not logged in" });
+    }
 });
  
 const PORT =5000;
